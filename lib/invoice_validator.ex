24b202ef2a62ba476995de73d085145c3618c231
Agustín Ramos <zentimental.software@gmail.com>
2022-03-24 00:19:15 -0600

    refactored invoice validator implementation to use Timex.Interval

--- lib/invoice_validator.ex
+++ lib/invoice_validator.ex
@@ -1,25 +1,22 @@
 defmodule InvoiceValidator do
-
   @secs_in_hour 60 * 60
   @secs_in_min 60
 
   def validate_dates(%DateTime{} = emisor_dt, %DateTime{} = pac_dt) do
-    case DateTime.compare(emisor_dt, pac_dt) do
-      :eq -> :ok
-      :lt ->
-        upper_bound_emisor_dt = DateTime.add(emisor_dt, 72 * @secs_in_hour, :second)
-        case DateTime.compare(upper_bound_emisor_dt, pac_dt) do
-          :eq -> :ok
-          :gt -> :ok
-          :lt -> {:error, :before_72_hrs}
-        end
-      :gt ->
-        upper_bound_emisor_dt = DateTime.add(emisor_dt, -5 * @secs_in_min, :second)
-        case DateTime.compare(upper_bound_emisor_dt, pac_dt) do
-          :eq -> :ok
-          :gt -> {:error, :after_5_min}
-          :lt -> :ok
-        end
+    lower_bound = DateTime.add(pac_dt, -72 * @secs_in_hour, :second)
+    upper_bound = DateTime.add(pac_dt, 5 * @secs_in_min, :second)
+
+    interval =
+      Timex.Interval.new(
+        from: lower_bound, until: upper_bound, left_open: false, right_open: false)
+
+    if emisor_dt in interval do
+      :ok
+    else
+      cond do
+        DateTime.compare(emisor_dt, lower_bound) == :lt -> {:error, :before_72_hrs}
+        DateTime.compare(emisor_dt, upper_bound) == :gt -> {:error, :after_5_min}
+      end
     end
   end
 end
