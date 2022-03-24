e81bec638d4d47f169d3ced82aced2b792a8d3e8
Agustín Ramos <zentimental.software@gmail.com>
2022-03-24 00:40:34 -0600

    refactored applying 'single level of abstraction' principle to 'validate_dates'

--- lib/invoice_validator.ex
+++ lib/invoice_validator.ex
@@ -1,8 +1,29 @@
 defmodule InvoiceValidator do
-  @secs_in_hour 60 * 60
   @secs_in_min 60
+  @secs_in_hour 60 * @secs_in_min
 
   def validate_dates(%DateTime{} = emisor_dt, %DateTime{} = pac_dt) do
+    {interval, lower_bound, upper_bound} = valid_interval(pac_dt)
+
+    if emisor_dt in interval do
+      :ok
+    else
+      cond do
+        lesser_than(emisor_dt, lower_bound) -> {:error, :before_72_hrs}
+        greater_than(emisor_dt, upper_bound) -> {:error, :after_5_min}
+      end
+    end
+  end
+
+  defp lesser_than(emisor_dt, lower_bound) do
+    DateTime.compare(emisor_dt, lower_bound) == :lt
+  end
+
+  defp greater_than(emisor_dt, upper_bound) do
+    DateTime.compare(emisor_dt, upper_bound) == :gt
+  end
+
+  defp valid_interval(pac_dt) do
     lower_bound = DateTime.add(pac_dt, -72 * @secs_in_hour, :second)
     upper_bound = DateTime.add(pac_dt, 5 * @secs_in_min, :second)
 
@@ -10,13 +31,6 @@ defmodule InvoiceValidator do
       Timex.Interval.new(
         from: lower_bound, until: upper_bound, left_open: false, right_open: false)
 
-    if emisor_dt in interval do
-      :ok
-    else
-      cond do
-        DateTime.compare(emisor_dt, lower_bound) == :lt -> {:error, :before_72_hrs}
-        DateTime.compare(emisor_dt, upper_bound) == :gt -> {:error, :after_5_min}
-      end
-    end
+    {interval, lower_bound, upper_bound}
   end
 end
