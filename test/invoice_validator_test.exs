1deb4245c445de2fd0fcf18684f48b6551e463f4
Agustín Ramos <zentimental.software@gmail.com>
2022-03-23 18:26:18 -0600

    test refactor: avoid code repetition

--- test/invoice_validator_test.exs
+++ test/invoice_validator_test.exs
@@ -10,20 +10,24 @@ defmodule InvoiceValidatorTest do
   @tz_pacific "America/Mazatlan"
 
   test "Emisor DateTimes equal to PAC dates are valid" do
-    emisor_dt = DateTime.from_naive!(~N[2022-03-24 10:00:00], @tz_cdmx)
-    pac_dt = DateTime.from_naive!(~N[2022-03-24 10:00:00], @tz_cdmx)
+    emisor_dt = datetime(~N[2022-03-24 10:00:00], @tz_cdmx)
+    pac_dt = datetime(~N[2022-03-24 10:00:00], @tz_cdmx)
     assert validate_dates(emisor_dt, pac_dt) == :ok
 
-    emisor_dt = DateTime.from_naive!(~N[2022-03-24 09:00:00], @tz_pacific)
-    pac_dt = DateTime.from_naive!(~N[2022-03-24 10:00:00], @tz_cdmx)
+    emisor_dt = datetime(~N[2022-03-24 09:00:00], @tz_pacific)
+    pac_dt = datetime(~N[2022-03-24 10:00:00], @tz_cdmx)
     assert validate_dates(emisor_dt, pac_dt) == :ok
 
-    emisor_dt = DateTime.from_naive!(~N[2022-03-24 09:00:00], @tz_northwest)
-    pac_dt = DateTime.from_naive!(~N[2022-03-24 10:00:00], @tz_cdmx)
+    emisor_dt = datetime(~N[2022-03-24 09:00:00], @tz_northwest)
+    pac_dt = datetime(~N[2022-03-24 10:00:00], @tz_cdmx)
     assert validate_dates(emisor_dt, pac_dt) == :ok
 
-    emisor_dt = DateTime.from_naive!(~N[2022-03-24 11:00:00], @tz_southeast)
-    pac_dt = DateTime.from_naive!(~N[2022-03-24 10:00:00], @tz_cdmx)
+    emisor_dt = datetime(~N[2022-03-24 11:00:00], @tz_southeast)
+    pac_dt = datetime(~N[2022-03-24 10:00:00], @tz_cdmx)
     assert validate_dates(emisor_dt, pac_dt) == :ok
+  end
+
+  defp datetime(%NaiveDateTime{} = ndt, tz) do
+    DateTime.from_naive!(ndt, tz)
   end
 end
